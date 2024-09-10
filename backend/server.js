aa2be9bef27444738ea9daa8c50ea571736c320d
Pavan Kamthane <95903010+Pavan-Kamthane@users.noreply.github.com>
2024-09-10 22:02:32 +0530

    2 Creating Node JS Server and Express JS API

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,34 @@
 const express =  require('express');
+const chats = require('./data/data.js');
+const dotenv = require('dotenv');
 
 const app = express()
+dotenv.config();
 
-app.listen(5000, console.log('Server is running on port 5000'))
+
+app.get("/",(req,res)=>{
+    res.send("App is running")
+})
+
+app.get("/api/chat",(req,res)=>{
+    res.send(chats)
+})
+
+// getting a specific chat from _id
+
+app.get("/api/chat/:id",(req,res)=>{
+    // console.log(req.params.id)
+    const singleChat = chats.find(
+        // meaning of (c) => c._id ===  req.params.id is  that we are looking for a chat object where the id is equal to the id in the url
+
+        (c) => c._id ===  req.params.id //  req.params.id is the id from the url and    
+
+    )
+
+    res.send(singleChat);
+})
+
+const PORT  =  process.env.PORT  || 5000
+
+
+app.listen(PORT, console.log(`Server is running on port http://localhost:${PORT}`))
